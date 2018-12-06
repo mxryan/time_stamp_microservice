440148fd9307b1d9dd9a820a9e7a2ec00864cc91
M Ryan <mxryan@live.com>
2018-12-06 08:39:42 -0500

    api routes apparently functional

--- server.js
+++ server.js
@@ -5,12 +5,45 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+// used to validate the output of parseInt()
+// parseInt(12-12-12) returns 12 but its digit count is not equal to "12-12-12".length
+// parseInt(12) also returns 12, but its digit count IS equal to "12".length
+const countDigits = (d) => {
+  let count = 0;
+  while (d > 0) {
+    count++;
+    d = Math.floor(d / 10);
+  }
+  return count;
+}
+
 app.get("/api/timestamp/", (req, res) => {
-  res.json({msg: "you should get current date back"});
-})
+  let now = new Date();
+  res.json({
+    "unix": now.getTime(),
+    "utc": now.toUTCString()
+  });
+});
 
 app.get("/api/timestamp/:date_string", (req, res) => {
-  res.json({msg:"hey", param: req.params.date_string});
-})
+  let dateString = req.params.date_string;
+  let date, serverResponse;
+  if (parseInt(dateString) && countDigits(parseInt(dateString)) === dateString.length){
+    date = new Date(parseInt(dateString));
+    serverResponse = {
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    }
+  } else if (Date.parse(dateString)) {
+    date = new Date(dateString);
+    serverResponse = {
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    }
+  } else {
+    serverResponse = {"error": "Invalid Date"}
+  }
+  res.json(serverResponse);
+});
 
 app.listen(PORT, ()=>console.log("server is on"));
